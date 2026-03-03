4287737ad26ba9cb8084a855f8b029b4c17625df
vivek chamoli <vivekchamoli153@gmail.com>
2026-03-03 15:18:16 +0530

    database connected

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+
+app.listen(8080,()=>{
+    console.log("Port is listening ");
+})
+
+const MONGO_URL="mongodb://127.0.0.1:27017/medico" 
+
+main().then(()=>{
+    console.log("Database connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/",(req,res)=>{
+    res.send("Root");
+})
